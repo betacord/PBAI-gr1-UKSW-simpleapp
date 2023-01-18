28af9a90c9b6844f3e7d3a1995b6c4750d2db6b7
tomek <t-krzywicki@wp.pl>
2023-01-18 17:23:51 +0100

    dodany post

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.event import Event
+from resources.event import Event, EventList
 
 app = Flask(__name__)
 api = Api(app)
 
 # GET: 127.0.0.1:5000/event/1
 api.add_resource(Event, '/event/<int:id_>')
+api.add_resource(EventList, '/events')  # 127.0.0.1:5000/events
 
 if __name__ == '__main__':
     app.run(port=5000)
--- resources/event.py
+++ resources/event.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from flask import request
 
 
 class Event(Resource):
@@ -11,3 +12,29 @@ class Event(Resource):
         }
 
         return event
+
+    @classmethod
+    def post(cls, id_: int) -> any:
+        event_json = request.get_json()
+        event_json['id'] = id_
+
+        return event_json, 201
+
+
+class EventList(Resource):
+    @classmethod
+    def get(cls) -> any:
+        return {
+            'events': [
+                {
+                    'id': 0,
+                    'name': 'test 1',
+                    'description': 'test description 1',
+                },
+                {
+                    'id': 1,
+                    'name': 'test 2',
+                    'description': 'test description 2',
+                },
+            ],
+        }
